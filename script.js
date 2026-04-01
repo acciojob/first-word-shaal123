2c9d99b9c50ab80f19b153ec596a46a90df21c83
Shalini  V <105047114+shaal123@users.noreply.github.com>
2026-04-01 20:28:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
-  // your code here
+  // Remove leading and trailing spaces
+  s = s.trim();
+
+  // Find index of first space
+  const index = s.indexOf(' ');
+
+  // If no space found, return whole string
+  if (index === -1) {
+    return s;
+  }
+
+  // Return substring from start to first space
+  return s.slice(0, index);
 }
 
 // Do not change the code below
